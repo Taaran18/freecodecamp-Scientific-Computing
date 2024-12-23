0c75d3c034bd1e3a395f4a606dfab44d0e31be15
Taaran Jain <taaranjain@Taarans-MacBook-Air.local>
2024-12-23 15:30:42 +0530

    Final

---
+++ Arithmetic Formatter/arithmetic_arranger.py
@@ -0,0 +1,81 @@
+def arithmetic_arranger(problem_list, show_answer=False):
+    if (len(problem_list)) > 5:
+        return "Error: Too many problems."
+
+    string = ""
+    problem_maxlen, first_row, sign, second_row = [], [], [], []
+
+    for maxlen in range(len(problem_list)):
+        temp = problem_list[maxlen].split()
+
+        if len(temp[0]) > 4 or len(temp[2]) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+        if not temp[0].isdigit() or not temp[2].isdigit():
+            return "Error: Numbers must only contain digits."
+
+        if temp[1] != "+" and temp[1] != "-":
+            return "Error: Operator must be '+' or '-'."
+
+        first_row.append(temp[0])
+        sign.append(temp[1])
+        second_row.append(temp[2])
+        problem_maxlen.append(max(len(temp[0]), len(temp[2])))
+
+    string += "  "
+    for index in range(len(problem_list)):
+        if index != len(problem_list) - 1:
+            string += (
+                (problem_maxlen[index] - len(first_row[index])) * " "
+                + first_row[index]
+                + "      "
+            )
+        else:
+            string += (
+                (problem_maxlen[index] - len(first_row[index])) * " "
+                + first_row[index]
+                + "\n"
+            )
+
+    for index in range(len(problem_list)):
+        if index != len(problem_list) - 1:
+            string += (
+                sign[index]
+                + " "
+                + (problem_maxlen[index] - len(second_row[index])) * " "
+                + second_row[index]
+                + "    "
+            )
+        else:
+            string += (
+                sign[index]
+                + " "
+                + (problem_maxlen[index] - len(second_row[index])) * " "
+                + second_row[index]
+                + "\n"
+            )
+
+    for index in range(len(problem_list)):
+        if index != len(problem_list) - 1:
+            string += "--" + problem_maxlen[index] * "-" + "    "
+        else:
+            if show_answer == False:
+                string += "--" + problem_maxlen[index] * "-"
+            else:
+                string += "--" + problem_maxlen[index] * "-" + "\n"
+
+    if show_answer == True:
+        for index in range(len(problem_list)):
+            if index != len(problem_list) - 1:
+                string += (
+                    (2 + problem_maxlen[index] - len(str(eval(problem_list[index]))))
+                    * " "
+                    + str(eval(problem_list[index]))
+                    + "    "
+                )
+            else:
+                string += (
+                    2 + problem_maxlen[index] - len(str(eval(problem_list[index])))
+                ) * " " + str(eval(problem_list[index]))
+
+    return string
